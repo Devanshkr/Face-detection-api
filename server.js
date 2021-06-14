d22d507af927ab5fb7803f5cd51f3d2794953a6c
devanshkr <devanshkumar2000dk@gmail.com>
2021-06-14 20:42:46 +0530

    adding dynamic port

--- server.js
+++ server.js
@@ -96,8 +96,8 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)} )
 //API call through the backend
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, ()=> {
-  console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000 , ()=> {
+  console.log(`app is running on port ${process.env.PORT} 3000`);
 });
 
 /* 
