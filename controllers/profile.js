f6a03ba1131f19440e2895840c6f9c22d9849c79
Devansh Kumar <59093441+Devanshkr@users.noreply.github.com>
2021-06-17 11:37:30 +0530

    Update profile.js

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,16 +1,7 @@
 const handleProfile = (req, res, db) => {
-  //req.PARAMS ye postman ka hai feature
   const { id } = req.params;
   let found = false;
-  // commented bc now using database postgres
-  // database.user.forEach(user => {
-  //   if(user.id === id){
-  //     found = true;
-  //     return res.json(user);
-  //   }
-  // })
   db.select('*').from('users').where({ id: id }).then(user => {
-    // console.log(user[0])
     if (user.length) {
       res.json(user[0]);
     } else {
@@ -20,9 +11,6 @@ const handleProfile = (req, res, db) => {
     .catch(err => {
       res.status(400).json("Error getting user");
     })
-  // if(!found) {
-  //   res.status(404).json('Not found');
-  // }
 }
 module.exports = {
   handleProfile: handleProfile
